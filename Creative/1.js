72203190c5978718b7c3067fd555edd44b1594f2
Anh <101346598+21T1@users.noreply.github.com>
2023-10-04 22:37:41 +0700

    Update 1.js

--- Creative/1.js
+++ Creative/1.js
@@ -1,4 +1,17 @@
-﻿ $(function(){
- 
-})  
- 
+$(function(){
+	$(window).scroll(function(){
+		if($('html').scrollTop() > 0){
+			$('.navbar-brand').addClass('text-black');
+			$('.navbar-nav').addClass('text-black');
+			$('.navbar').addClass('bg-white')
+			.removeClass('navbar-dark')
+			.addClass('navbar-light');
+		}else{
+			$('.navbar-brand').removeClass('text-black');
+			$('.navbar-nav').removeClass('text-black');
+			$('.navbar').removeClass('bg-white')
+			.addClass('navbar-dark')
+			.remoceClass('navbar-light');
+		}
+	}) 
+})
