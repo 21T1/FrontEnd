a7a076cdf70f0ebd52fb1fd2b5e40731feb7e856
Anh <21T1hoanganh@gmail.com>
2023-10-05 22:33:57 +0700

    Update 1.js

--- Creative/1.js
+++ Creative/1.js
@@ -1,17 +1,19 @@
 $(function(){
 	$(window).scroll(function(){
-		if($('html').scrollTop() > 0){
-			$('.navbar-brand').addClass('text-black');
-			$('.navbar-nav').addClass('text-black');
-			$('.navbar').addClass('bg-white')
-			.removeClass('navbar-dark')
-			.addClass('navbar-light');
-		}else{
-			$('.navbar-brand').removeClass('text-black');
-			$('.navbar-nav').removeClass('text-black');
-			$('.navbar').removeClass('bg-white')
-			.addClass('navbar-dark')
-			.remoceClass('navbar-light');
+		if(screen.width >= 992){
+			if($('html').scrollTop() > 0){
+				$('.navbar-brand').addClass('text-black');
+				$('.navbar-nav').addClass('text-black');
+				$('.navbar').addClass('bg-white')
+				.removeClass('navbar-dark')
+				.addClass('navbar-light');
+			}else{
+				$('.navbar-brand').removeClass('text-black');
+				$('.navbar-nav').removeClass('text-black');
+				$('.navbar').removeClass('bg-white')
+				.addClass('navbar-dark')
+				.remoceClass('navbar-light');
+			}
 		}
 	}) 
 })
