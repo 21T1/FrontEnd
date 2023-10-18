6bfaf331980b6a1bfe52f19192b6e6745b0a55cc
Anh <21T1hoanganh@gmail.com>
2023-10-18 22:15:22 +0700

    Update 1.js

--- Velocity/1.js
+++ Velocity/1.js
@@ -1,4 +1,23 @@
-﻿ $(function(){
- 
+﻿$(function(){
+	$(window).scroll(function(){
+		if($('html').scrollTop() > 0){
+			$('.logo').addClass('change-logo');
+			$('.navbar-dark').removeClass('bg-transparent').addClass('bg-light');
+			$('.menu').addClass('change-menu');
+			$('.scroll-top').removeClass('hidden');
+		}else{
+			$('.logo').removeClass('change-logo');
+			$('.navbar-dark').addClass('bg-transparent').removeClass('bg-light');
+			$('.menu').removeClass('change-menu');
+			$('.scroll-top').addClass('hidden');
+		}
+	})
+
+	$('.scroll-top').click(function(){
+		$('html').animate({
+			'scrollTop': 0
+		});
+		return false;
+	})
 })  
  
